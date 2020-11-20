655d752329de0f223dee43dc0879add40aff385c
muhammad mufti ramdhani <muftiramdhani25@gmail.com>
2020-11-20 08:31:34 +0700

    starter project

--- Hijaiyah/ViewController.swift
+++ Hijaiyah/ViewController.swift
@@ -6,12 +6,9 @@
 //
 
 import UIKit
-import AVFoundation
 
 class ViewController: UIViewController {
     
-    var player: AVAudioPlayer?
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,15 +16,12 @@ class ViewController: UIViewController {
 
 
     @IBAction func btnHijaiyah(_ sender: UIButton) {
-        playSound(sound: sender.currentTitle!)
+        
         
     }
     
     func playSound(sound: String) {
-        let call = Bundle.main.url(forResource: sound, withExtension: "mp3")
-        player = try! AVAudioPlayer(contentsOf: call!)
         
-        player?.play()
     }
 }
 
